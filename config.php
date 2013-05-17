25b3cb2336b80cb635d27065fc134030635b478b
Matthias Tilsner <matthias@tilsner.eu>
2013-05-17 16:25:15 +0200

    patched the Google_CurlIO class
    Added means to curl directly to file rather than to stdin

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+define(NEXUS_FOLDER, "0B2gEy70FraS_NzRkYzVmMzctZTE4ZC00NDdhLTlhMjEtZTdmMDUwMzVjYWFl");
+define(FILE_DIR, "/share/Multimedia/Nexus");
+define(HASH_DIR, "/share/Multimedia/Nexus/.hashes");
+define(CLIENT_ID, "933712632234-7aobpdpjb1m69vs83adg6e4756j3fque.apps.googleusercontent.com");
+define(CLIENT_SECRET, "zhwjeskpyQWJ1rmygPwtIbP4");
+define(AUTH_TOKEN_FILE, "token.json");
+define(MAX_PROCESS_TIME, "-3 days");
+define(FILE_OWNER, "matthias:everyone");
+
+?>
